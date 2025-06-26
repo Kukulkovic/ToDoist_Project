fd38d0398f7c827733269758d3fa1b6e01a87ad7
pkuk <piotr.kukulka@inetum.com>
2025-06-26 21:16:45 +0200

    26-06e

--- tests/smoke/create-project.spec.ts
+++ tests/smoke/create-project.spec.ts
@@ -6,27 +6,9 @@ test('should create a new project', { tag: ['@project', '@wip'] }, async ({ page
 
   const homePage = new HomePage(page);
 
-  await homePage.open();  // Navigate to the Todoist app
+  await homePage.open();
+  await homePage.leftPanel.addNewProject('Test project', 'Łosoś');
 
-  await homePage.leftPanel.openProjectsMenu();  // Open the projects menu
-
-  const addProjectForm = page.getByTestId('modal-overlay').locator('form');
-  await expect(addProjectForm).toBeVisible();
-
-  await addProjectForm.locator('input[name = "name"]').fill('Test project');
-
-  // const colorPicker = addProjectForm.locator('button[aria-labelledby="edit_project_modal_field_color_label"]');
-  const colorPicker = addProjectForm.getByRole('combobox').first();
-  await expect(colorPicker).toBeVisible();
-  await colorPicker.click();
-
-  // const projectColorSelector = page.locator('div .popper');
-  // const projectColorSelector = page.locator('.form_field div[role=dialog]');
-  const projectColorSelector = page.getByRole('listbox');
-  await expect(projectColorSelector).toBeVisible();
-
-  await projectColorSelector.getByText('Intensywny czerwony').click();
-  await addProjectForm.getByRole('button', { name: 'Dodaj' }).click();
 
   const projectList = page.locator('#projects_list');
   await expect(projectList.locator('li').first()).toHaveText('Test project');
